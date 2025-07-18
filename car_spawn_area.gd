3bbda618fcccd3d645868a1c254b78d6299e6e1d
Heliguy <heliguy4599@hotmail.com>
2025-07-17 21:54:59 -0400

    Attempt to add car spawn area

---
+++ car_spawn_area.gd
@@ -0,0 +1,25 @@
+class_name CarSpawnArea
+extends Area2D
+
+const CAR_SCENE: PackedScene = preload("res://car.tscn")
+const exited_cars: Array[Car] = []
+
+@export var marker: Marker2D = null
+
+@onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	timer.timeout.connect(spawn_car)
+	body_exited.connect(func (body: Node2D): 
+		if body is Car and not body in exited_cars:
+			timer.start()
+			exited_cars.append(body)
+	)
+
+
+func spawn_car() -> void:
+	var car = CAR_SCENE.instantiate()
+	print(car.position)
+	car.global_position = marker.global_position if marker != null else global_position
+	(marker.add_sibling if marker != null else add_sibling).call(car)
--- levels/level_1.gd
+++ levels/level_1.gd
@@ -1,12 +1,8 @@
 class_name Level1
 extends StaticBody2D
 
-const CAR_SCENE: PackedScene = preload("res://car.tscn")
-
 @onready var car_win_area: Area2D = $CarWinArea
-@onready var car_spawn_area: Area2D = $CarSpawnArea
-@onready var car_spawn_marker: Marker2D = $CarSpawnArea/CarSpawnMarker
-@onready var timer: Timer = $Timer
+@onready var car_spawn_area: CarSpawnArea = $CarSpawnArea
 
 var cars_deleted := 0
 var exited_cars: Array[Car] = []
@@ -19,15 +15,4 @@ func _ready() -> void:
 			cars_deleted += 1
 			print("Cars Deleted: ", cars_deleted)
 	)
-	timer.timeout.connect(_spawn_car)
-	car_spawn_area.body_exited.connect(func (body: Node2D):
-		if body is Car and not body in exited_cars:
-			timer.start()
-	)
-	_spawn_car()
-
-
-func _spawn_car() -> void:
-	var car: Car = CAR_SCENE.instantiate()
-	car.global_position = car_spawn_marker.global_position
-	add_child(car)
+	car_spawn_area.spawn_car()
